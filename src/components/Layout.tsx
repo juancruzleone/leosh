828c74f556da678a1c0235b43444db748bfe188c
Juan cruz Leone <juancruzleone@outlook.com>
2024-05-27 23:26:42 -0300

    Se crearon los componentes para h1, nav y footer

---
+++ src/components/Layout.tsx
@@ -0,0 +1,10 @@
+const Layout = () => {
+    return(
+        <>
+        <nav>
+        </nav>
+        </>
+    )
+}
+
+export default Layout
---
+++ src/components/MainH1.tsx
@@ -0,0 +1,9 @@
+const MainH1 = () => {
+    return(
+        <>
+        <h1></h1>
+        </>
+    )
+}
+
+export default MainH1
